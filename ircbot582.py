401e35d873c09a13dbaef25570e1a668df195861
Logan Williams <loganwilliams116@gmail.com>
2016-11-28 13:35:04 -0800

    cleaned up old chatbot stuff

--- ircbot582.py
+++ ircbot582.py
@@ -4,7 +4,7 @@
 #
 # Joel Rosdahl <joel@rosdahl.net>
 # slight modifications by Foaad Khosmood
-# modified for CPLUG questions by Logan Williams
+# modified for CPE 582 Lab 4 by Logan Williams and Justin Postigo
 #   based on info from http://cplug.org/about/a-brief-history-of-cplug/
 #
 # command to add bot to irc: python3 ircbot582.py irc.freenode.net "#CPE582" botname 
@@ -31,64 +31,13 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+initial_greetings = ["hello", "hey", "hi", "what's up", "how's it going", "how are you"]
+
 class TestBot(irc.bot.SingleServerIRCBot):
-    url = "http://cplug.org/about/a-brief-history-of-cplug/"
-    years = {}
 
     def __init__(self, channel, nickname, server, port=6667):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
-        self.years_pos_people = {} 
-        self.update()
-           
-    # Takes in a string like "2003-2005" and returns a list
-    # like [2003, 2004, 2005]
-    def yr_range_to_list(self, yr_range):
-        begin = int(yr_range.split('-')[0])
-        end = int(yr_range.split('-')[1])
-        yr_list = []
-        yr_list.append(begin)
-        cur = begin
-        while (cur < end):
-            cur += 1
-            yr_list.append(cur)
-        yr_list.append(end)
-
-        return yr_list
-
-    def update(self):
-        # scrapes the url and updates self.data
-        myRequest = requests.get(self.url)
-        soup = BeautifulSoup(myRequest.text, "html.parser")
-        cur_p = soup.find('p')
-        
-        cur_p = cur_p.find_next('p')
-        while (cur_p != None):
-           years = cur_p.contents[0]
-           match = re.search('^(\d+-\d+)', years)
-           if (match == None):
-              print("ERROR: regex match on years somehow got None")
-           else:
-              years = match.group(1)
-           
-           pos_person_dict = {}
-           cur_p = cur_p.find_next('ul')
-           pre_people = list(filter(lambda a: a != '\n', cur_p.contents))
-           for pos_person in pre_people:
-              pos_person = pos_person.string
-              #pos_person = pos_person.replace('â', '\'')
-              pos_person = pos_person.replace('<li>', '')
-              pos_person = pos_person.replace('</li>', '')
-              splitted = pos_person.split(':')
-              if (len(splitted) == 2):
-                 pos_person_dict[splitted[0].lower().strip()] = splitted[1].lower().strip()
-           
-           if (len(pos_person_dict) > 0):
-              for yr in self.yr_range_to_list(years):
-                  self.years_pos_people[yr] = pos_person_dict
-           
-           cur_p = cur_p.find_next('p')
-
 
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
@@ -145,39 +94,10 @@ class TestBot(irc.bot.SingleServerIRCBot):
             c.ctcp("DCC", nick, "CHAT chat %s %d" % (
                 ip_quad_to_numstr(dcc.localaddress),
                 dcc.localport))
-        elif re.search('who was ([\w ]+) in (\d+)?', cmd.lower()): 
-            match = re.search('who was ([\w ]+) in (\d+)?', cmd.lower())
-            if (match == None):
-                print("ERROR: expected match on who was question, got None")
-            else: 
-                pos = match.group(1).lower()
-                year = int(match.group(2))
-                if year not in self.years_pos_people:
-                    c.privmsg(self.channel, "Sorry, I only know info for years 2003-2016.")
-                else:
-                    if pos not in self.years_pos_people[year]:
-                        c.privmsg(self.channel, "Sorry, I don't know who was " + pos + " in " + str(year) + ".")
-                    else:
-                        person = self.years_pos_people[year][pos]
-                        c.privmsg(self.channel, str(person).title() + " was " + pos + " in " + str(year) + ".")
-        elif re.search('when was ([\w ]+) the ([\w ]+)?', cmd.lower()): 
-            match = re.search('when was ([\w ]+) the ([\w ]+)?', cmd.lower())
-            person = match.group(1).lower()
-            pos = match.group(2).lower()
-            years = []
-            for yr in self.years_pos_people.keys():
-                if pos in self.years_pos_people[yr]:
-                    if person == self.years_pos_people[yr][pos]:
-                        years.append(yr)
-            if len(years) > 0:
-                result_msg = str(person).title() + " was " + pos + " in " + str(years).replace('[', '').replace(']', '') + "."
-            else: 
-                result_msg = "It looks like " + person.title() + " was never the " + pos + "."
-            c.privmsg(self.channel, result_msg)
+        elif cmd in initial_greetings:
+            c.notice(nick, cmd + " back to you!")
         elif cmd == "about":
-            c.privmsg(self.channel, "I was made by Logan Williams for Foaad Khosmood for the CPE 466 class in Spring 2016")
-        elif cmd == "usage":
-            c.privmsg(self.channel, "I can answer questions like this: \"Who was [CLUB_POSITION] in [YEAR]?\" or \"When was [NAME] the [CLUB_POSITION]?\" Some positions include: president, vice president, treasurer, secretary, and webmaster. Note: not every year has info for every position.") 
+            c.privmsg(self.channel, "I was made by Justin Postigo and Logan Williams for Foaad Khosmood for the CPE 582 class in Fall 2016.")
         else:
             c.notice(nick, "Not understood: " + cmd)
 
